02928d513fe79b11a68bf5e0a4a0216722da0d5b
Danniyalrj <166133285+Danniyalrj@users.noreply.github.com>
2024-04-17 18:53:35 +0500

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,12 @@
+function about(){
+    window.location = "About_us.html";
+}
+function Home1(){
+window.location = "index.html";
+}
+function news(){
+window.location = "News.html";
+}
+function Services(){
+    window.location = "services.html";
+    }
